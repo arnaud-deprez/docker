31f6bd5647722e98213301a061e68902a1be02c5
Arnaud Deprez <arnaud.deprez@lampiris.be>
2014-08-20 14:54:48 +0100

    use entrypoint the postgres image change file encoding

--- postgres/9.3/docker-entrypoint.sh
+++ postgres/9.3/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "$1" = 'postgres' ]; then
-	echo "in postgres"
-	/usr/lib/postgresql/$PG_VERSION/bin/postgres -D /var/lib/postgresql/$PG_VERSION/main -c config_file=/etc/postgresql/$PG_VERSION/main/postgresql.conf
-	echo "end of postgres"
+set -e
+
+if [ "$1" == "postgres" ]; then
+	exec gosu postgres "$@"
 fi
 
-#exec "$@"
+exec "$@"
