98fcf987f8beaeeb0c3777e6b96b5ff0d06301e4
Arnaud Deprez <arnaud.deprez@lampiris.be>
2014-12-01 15:06:55 +0100

    update postgres script

--- postgres/9.3/init-docker-schema.sh
+++ postgres/9.3/init-docker-schema.sh
@@ -5,7 +5,7 @@
 # Allow prepared transactions to enable XA transactions
 # Disable ssl configuration
 echo "******UPDATE CONFIGURATION******"
-sed -ri "s/^(host all all 0.0.0.0\/0) trust/\1 md5/" "$PGDATA"/pg_hba.conf
+#sed -ri "s/^(host all all 0.0.0.0\/0) trust/\1 md5/" "$PGDATA"/pg_hba.conf
 sed -ri "s/^#(max_prepared_transactions\s*=\s*)\S+/\120/" "$PGDATA"/postgresql.conf
 sed -e "s/^ssl\s*=\s*true/ssl = false/" -i "$PGDATA"/postgresql.conf
 echo ""
@@ -13,7 +13,6 @@ echo "******CONFIGURATION UPDATED******"
 
 echo "******CREATING DOCKER DATABASE******"
 gosu postgres postgres --single <<- EOSQL
-   ALTER USER "postgres" WITH SUPERUSER ENCRYPTED PASSWORD 'postgres';
    CREATE DATABASE "docker";
    CREATE USER "docker";
    ALTER USER "docker" WITH ENCRYPTED PASSWORD 'docker';
